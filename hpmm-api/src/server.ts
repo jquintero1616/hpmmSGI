a867566b9f00f42d170b1d9b456c1b768bba06a3
jquintero1616 <ardomjoze1116@gmail.com>
2025-10-30 14:17:35 -0600

    Last test, moving to 443 port.

--- hpmm-api/src/server.ts
+++ hpmm-api/src/server.ts
@@ -9,11 +9,12 @@ import path from "path";
 // 1) Habilitamos trust proxy para que Express lea X-Forwarded-For
 app.set("trust proxy", true);
 
+
 const USE_HTTPS = process.env.USE_HTTPS === "true";
-let portToUse = parseInt(process.env.PORT ?? "3000", 10) || 3000;
+let portToUse = parseInt(process.env.PORT ?? (USE_HTTPS ? "8443" : "3000"), 10) || (USE_HTTPS ? 8443 : 3000);
+
 if (USE_HTTPS) {
-  portToUse = 443;
-  // PRODUCCIÓN - HTTPS
+  // PRODUCCIÓN - HTTPS (puerto alto para no-root)
   const key = fs.readFileSync(path.resolve(__dirname, "../certs/key.pem"));
   const cert = fs.readFileSync(path.resolve(__dirname, "../certs/cert.pem"));
   https.createServer({ key, cert }, app).listen(portToUse, "0.0.0.0", () => {
